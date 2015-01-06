ac45986603b0d297a3d7b6c29afe7521c8750303
Mouq <alexmoquin@gmail.com>
2015-01-06 12:49:03 -0500

    Get functional enough for a first test
    The major caveat right now: There's a bug where the file loading the
    slang needs to have a 'BEGIN { ... }' somewhere before the 'use'
    statement, otherwise Slangy can't find the slang.

--- lib/Slangy.pm
+++ lib/Slangy.pm
@@ -1,15 +1,18 @@
-grammar Slangy::Grammar {
-    token TOP { aaa }
-    token thing { bbb }
-}
+role Slangy::Grammar { }
+role Slangy::Actions { }
 
 class MetamodelX::SlangHOW is Metamodel::ClassHOW {
     has Mu $!grammar;
+    has Mu $!actions;
 
     method add_attribute(Mu \type, Mu \attribute) {
         if attribute.name eq '$!grammar' {
             $!grammar := attribute;
             #~ self.Metamodel::ClassHOW::add_method(type, 'grammar', $meth);
+        }
+        elsif attribute.name eq '$!actions' {
+            $!actions := attribute;
+            #~ self.Metamodel::ClassHOW::add_method(type, 'actions', $meth);
         }
         #~ else {
             self.Metamodel::ClassHOW::add_attribute(type, attribute);
@@ -40,11 +43,12 @@ multi EXPORT(|) {
 multi EXPORT($lang) {
     {
         '&EXPORT' => -> {
-            $*MAIN        := $lang;
-            say ::.keys;      # $_ $*DISPATCHER
-            say UNIT::.keys;  # $! $/ $_ GLOBALish EXPORT $?PACKAGE ::?PACKAGE Slangy MetamodelX EXPORTHOW &EXPORT $=pod !UNIT_MARKER
-            say OUTER::.keys; # $! $/ $_ $lang $*DISPATCHER &?ROUTINE RETURN
-            %*LANG{$lang} := GLOBAL.WHO{$lang}.new.grammar;
+            die "Could not find class '$lang'" unless GLOBAL.WHO{$lang}:exists;
+            my $lang-class = GLOBAL.WHO{$lang}.new;
+            die "'$lang' is not a slang" unless $lang-class.HOW ~~ MetamodelX::SlangHOW;
+            %*LANG{$lang} := $lang-class.grammar;
+            %*LANG{$lang~'-actions'} := $lang-class.actions;
+            $*MAIN := $lang;
             {}
         },
     }
